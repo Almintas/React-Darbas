07223e4271cccceef27315ad852f5f94827a2fa7
Almintas <almintasbruzas@gmail.com>
2022-10-25 20:38:15 +0300

    5th commit

--- src/App.js
+++ src/App.js
@@ -2,46 +2,35 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    console.log('COMPONENT');
-  }, [count]);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => {
+      setPosts(data);
+    })
+  }, [])
 
-  const handleIncrement = (event) => {
-    setCount(count + 1);
+  const handleChangeInput = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
   }
 
-  const handleIncrementKeyUp = (event) => {
-    console.log('UP');
-  }
-
-  const handleIncrementKeyDown = (event) => {
-    console.log('DOWN');
-  }
-
-  const handleInputChange = (event) => {
-    console.log(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('SUBMIT');
-  }
-
-  const handleBlur = (event) => {
-    console.log('BLUR');
-  }
-  
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        <button onKeyUp={handleIncrementKeyUp} 
-      onKeyDown={handleIncrementKeyDown} 
-      onClick={handleIncrement}>Increment</button>
-
-      <input onChange={handleInputChange} onBlur={handleBlur} />
-      </form>
+      <input onChange={handleChangeInput} />
+      {posts
+      .filter((post) => {
+        return post.title.indexOf(inputValue) >= 0
+      })
+      .map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
