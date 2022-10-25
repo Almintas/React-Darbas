8c96153cd51b4d631dd0263998cb196fa051b680
Almintas <almintasbruzas@gmail.com>
2022-10-25 18:56:57 +0300

    Lesson 5.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Counter } from './Counter/Counter';
 
 function App() {
-  const [isCounterVisible, setIsCounterVisible] = useState(true);
+  const [count, setCount] = useState(0);
 
-  const handleToggleCounter = () => {
-    setIsCounterVisible(!isCounterVisible);
+  useEffect(() => {
+    console.log('COMPONENT');
+  }, [count]);
+
+  const handleIncrement = (event) => {
+    setCount(count + 1);
   }
+
+  const handleIncrementKeyUp = (event) => {
+    console.log('UP');
+  }
+
+  const handleIncrementKeyDown = (event) => {
+    console.log('DOWN');
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('SUBMIT');
+  }
+
+  const handleBlur = (event) => {
+    console.log('BLUR');
+  }
+  
   return (
     <div className="App">
-      <button onClick={handleToggleCounter}>Toggle Counter</button>
-      {isCounterVisible && <Counter />}
-      <Counter />
+      <form onSubmit={handleSubmit}>
+        <button onKeyUp={handleIncrementKeyUp} 
+      onKeyDown={handleIncrementKeyDown} 
+      onClick={handleIncrement}>Increment</button>
+
+      <input onChange={handleInputChange} onBlur={handleBlur} />
+      </form>
     </div>
   );
 }
